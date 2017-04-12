42b733ae6a1de08fad19e0d2835fa270707c3a9e
Savannah Baron <savannah@WL-222-183.WPA.Claremont.Edu>
2017-04-11 20:17:59 -0700

    Added log reg experiments

--- full_ibc/runKFold.py
+++ full_ibc/runKFold.py
@@ -1,24 +1,28 @@
 import numpy as np
 import cPickle
+import time
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 import full_ibc.treeUtil as treeUtil
 import util.util as utilities
 import util.visualize as vis
 
 from experiments.test_experiment import randomModel
+import experiments.log_reg_experiments as logReg
 
 # Only change these variables (and the visualizations section if necessary)
 # List of functions that predict two classes
-twoClass = [randomModel]
-twoClassNames = ["Random2Class"]
+twoClass = [randomModel, logReg.simpleLogRegModel, logReg.simpleTfidfLogRegModel] + [logReg.paramLogReg for x in logReg.kwargsList]
+twoClassNames = ["Random2Class", "Simple Log Reg", "Simple Tfidf Log Reg"] + logReg.nameList
+twoClassKwargs = [None, None, None] + logReg.kwargsList
 # List of functions that predict three classes
 threeClass = [randomModel]
 threeClassNames = ["Random3Class"]
 
 def main():
 	# Load data
+	print len(twoClass)
 	path = utilities.findFileOnPath('ibcData.pkl')
 	[lib, con, neutral] = cPickle.load(open(path, 'rb'))
 
@@ -44,28 +48,6 @@ def main():
 	# Separate Test Set
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
 
-	# Oversample unbalanced data
-	neutral_indices = np.where(y_train == 0)
-	con_indices = np.where(y_train == -1)
-	lib_indices = np.where(y_train == 1)
-	lib_len = len(lib_indices[0])
-	con_len = len(con_indices[0])
-	neutral_len = len(neutral_indices[0])
-	goalSize = max(neutral_len, con_len, lib_len)
-	np.random.seed(42)
-	X_train = np.append(X_train, np.random.choice(X_train[neutral_indices], 
-		                                          size=(goalSize - neutral_len),
-		                                          replace=True))
-	y_train = np.append(y_train, np.repeat(0, goalSize-neutral_len))
-	X_train = np.append(X_train, np.random.choice(X_train[con_indices], 
-		                                          size=(goalSize - con_len),
-		                                          replace=True))
-	y_train = np.append(y_train, np.repeat(-1, goalSize - con_len))
-	X_train = np.append(X_train, np.random.choice(X_train[lib_indices], 
-		                                          size=(goalSize-lib_len),
-		                                          replace=True))
-	y_train = np.append(y_train, np.repeat(1, goalSize-lib_len))
-
 	numSplits = 10
 	kFold = StratifiedKFold(n_splits=numSplits, shuffle=True, random_state=42)
 	y_pred_2_all = []
@@ -87,9 +69,11 @@ def main():
 
 		y_pred_2_fold = []
 		y_pred_3_fold = []
-		for func in twoClass:
-			y_pred_2_fold.append(func(X_train_k_2, y_train_k_2, X_test_k_2, y_test_k_2))
-
+		for m in range(len(twoClass)):
+			if twoClassKwargs[m] != None:
+				y_pred_2_fold.append(twoClass[m](X_train_k_2, y_train_k_2, X_test_k_2, y_test_k_2, **twoClassKwargs[m]))
+			else:
+				y_pred_2_fold.append(twoClass[m](X_train_k_2, y_train_k_2, X_test_k_2, y_test_k_2))
 		for func in threeClass:
 			y_pred_3_fold.append(func(X_train_k_3, y_train_k_3, X_test_k_3, y_test_k_3))
 		y_pred_2_all.append(y_pred_2_fold)
@@ -97,19 +81,36 @@ def main():
 		y_true_2_all.append(y_test_k_2)
 		y_true_3_all.append(y_test_k_3)
 
-	# Evaluate
-
-	# Visualize
-	# Confusion Matrices for All Folds/Experiments
+	# Evaluate and Visualize
+	# Confusion Matrices and Accuracy for All Folds/Experiments
+	accuracy_all = []
 	for k in xrange(numSplits):
+		accuracy_temp = []
 		for m in xrange(len(twoClass)):
 			y_true = y_true_2_all[k]
 			y_pred = y_pred_2_all[k][m]
 			y_label = np.sign(y_pred)
+			y_label[y_label==0] = 1 # map points of hyperplane to +1
+			accuracy_temp.append(accuracy_score(y_true, y_label))
 			cm = confusion_matrix(y_true, y_label)
 			title = twoClassNames[m]
 			path = "../visualizations/{}/cm_fold{}.png".format(title, k)
 			vis.plotConfusionMatrix(cm, ["Conservative", "Liberal"], path=path, title=title)
+		accuracy_all.append(accuracy_temp)
+	for m in xrange(len(twoClass)):
+		accuracy = [accuracy_all[k][m] for k in xrange(numSplits)]
+		minimum = min(accuracy)
+		maximum = max(accuracy)
+		average = np.average(accuracy)
+		path = "../visualizations/accuracy_results.txt"
+		utilities.mkdir_p(path)
+		with open(path,"a+") as f:
+			toWrite = time.ctime() + ": " + twoClassNames[m] + "\n"
+			toWrite += "Min Accuracy: " + str(minimum) + "\n"
+			toWrite += "Max Accuracy: " + str(maximum) + "\n"
+			toWrite += "Ave Accuracy: " + str(average) + "\n"
+			f.write(toWrite)
+
 	# ROC Plots for All Experiments
 	for m in xrange(len(twoClass)):
 		y_true = y_true_2_all
