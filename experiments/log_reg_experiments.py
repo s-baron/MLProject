997fbca884bd5b073e75d5126e438ffc6fd5a60d
Savannah Baron <savannah@WL-222-183.WPA.Claremont.Edu>
2017-04-18 14:26:40 -0700

    Modified log reg to test biased n-grams. Running into import problem in runKFold

--- experiments/log_reg_experiments.py
+++ experiments/log_reg_experiments.py
@@ -38,6 +38,46 @@ def paramLogReg(X_train, y_train, X_test, y_test, **kwargs):
 	y_pred = clf.decision_function(test_dtm)
 	return y_pred
 
+def biasedNGramsLogRegModel(X_train, y_train, X_test, y_test):
+	# ---------------------------- Make Vocab ------------------------------
+	lib_indices = np.where(y_train == 1)
+	con_indices = np.where(y_train == -1)
+    X_lib = X_train[lib_indices]
+    X_con = X_train[con_indices]
+
+    dtms = []
+    vocabularies = []
+    for X in [X_lib, X_con]:
+        cv = CountVectorizer(ngram_range=(1, 3))
+        dtm = cv.fit_transform(X)
+        dtms.append(dtm)
+        vocabularies.append(cv.vocabulary_)
+    vocabSets = [set(vocab.keys()) for vocab in vocabularies]
+    dtms[0] = dtms[0].toarray()
+    dtms[1] = dtms[1].toarray()
+    wordsInBoth = vocabSets[0].intersection(vocabSets[1])
+    vocab = []
+    for word in wordsInBoth:
+        a = dtms[0][:, vocabularies[0][word]]
+        b = dtms[1][:, vocabularies[1][word]]
+        t, prob = stats.ttest_ind(a, b)
+        if prob < 0.1:
+            vocab.append(word)
+    print len(vocab)
+
+	# ---------------------------- Training Set ------------------------------
+	# Get words and their frequencies
+	cv = CountVectorizer(ngram_range=(1, 3), vocabulary=vocab)
+	cv.fit(X_train)
+	train_dtm = cv.transform(X_train)
+	clf = LogisticRegression(class_weight="balanced")
+	clf.fit(train_dtm, y_train)
+
+	# ---------------------------- Testing Set ------------------------------
+	test_dtm = cv.transform(X_test)
+	y_pred = clf.decision_function(test_dtm)
+	return y_pred
+
 def simpleLogRegModel(X_train, y_train, X_test, y_test):
 	# ---------------------------- Training Set ------------------------------
 	# Get words and their frequencies
